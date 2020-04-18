accedc8fdb9353db560545e43b856cf701243ced
Ryan Barwise <ryanbarwise@gmail.com>
2020-04-18 19:22:24 -0400

    save results function

--- quiz.js
+++ quiz.js
@@ -71,69 +71,78 @@ function quizTimer() {
 
 
 
-    function checkAnswer(event) {
-        if (event.target.tagName.toLowerCase() === 'button') {
-            if (event.target.textContent === questions[questionNum].answer) {
-                //this goes off if it is the correct answer
-                correct++
-            } else {
-                //this goes off if it is the wrong answer
-                incorrect++
-                secondsLeft = secondsLeft - 5
-            }
-            questionNum++
-            if (questionNum < questions.length) {
-                setQuestion();
-            } else {
-                showResults();
-
-                }
-                // call a function to show results and allows user to enter initials and save score...
-                // navigate to your high score html page which will show all of the high scores you have saved to your local storage...
-            }
-
-
-
+function checkAnswer(event) {
+    if (event.target.tagName.toLowerCase() === 'button') {
+        if (event.target.textContent === questions[questionNum].answer) {
+            //this goes off if it is the correct answer
+            correct++
+        } else {
+            //this goes off if it is the wrong answer
+            incorrect++
+            secondsLeft = secondsLeft - 5
         }
-
-        function showResults() {
-            quizDiv.style.display = "none";
-            var submit = document.getElementById('submit');
-            submit.textContent = correct + " right and " + incorrect + " wrong";
-            
-            var userInfo = document.getElementById('userInfo');
-            var initialsBox = document.createElement('input');
-            initialsBox.setAttribute('type', 'text');
-            initialsBox.setAttribute('value', 'user initials')
-            userInfo.appendChild(initialsBox);
-            
-            var submitBox = document.getElementById('submitBox');
-            var submitBoxEl = document.createElement('BUTTON');
-            submitBoxEl.textContent = "Save Score!";
-            submitBox.appendChild(submitBoxEl);
-       
+        questionNum++
+        if (questionNum < questions.length) {
+            setQuestion();
+        } else {
+            showResults();
         }
-    
-    
-
-
-
-
-    startButton.addEventListener('click', function () {
-        var instructions = document.getElementById('instructions');
-        instructions.style.display = 'none';
-        quizTimer();
-        setQuestion();
-
-    quizDiv.addEventListener('click', checkAnswer);
-
-    submitBoxEl.addEventListener('click', function () {
-        
+        // call a function to show results and allows user to enter initials and save score...
+        // navigate to your high score html page which will show all of the high scores you have saved to your local storage...
     }
+}
+
+function showResults() {
+    quizDiv.style.display = "none";
+    var submit = document.getElementById('submit');
+    submit.textContent = correct + " right and " + incorrect + " wrong";
+
+    var userInfo = document.getElementById('userInfo');
+    var initialsBox = document.createElement('input');
+    initialsBox.setAttribute('type', 'text');
+    initialsBox.setAttribute('placeholder', 'user initials')
+    userInfo.appendChild(initialsBox);
+
+    var submitBox = document.getElementById('submitBox');
+    var submitBoxEl = document.createElement('BUTTON');
+    submitBoxEl.textContent = "Save Score!";
+    submitBoxEl.addEventListener('click', function () {
+        saveResults({ correct, incorrect, initials: initialsBox.value })
 
     })
+    submitBox.appendChild(submitBoxEl);
+}
 
-    //quizDiv.addEventListener('click', checkAnswer);
-  
+
+function saveResults(obj) {
+
+    var highScores = localStorage.getItem('highScores');
+    if (highScores) {
+        highScores = JSON.parse(highScores)
+        highScores.push(obj)
+    } else {
+        highScores = [obj]
+    }
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location = '/highscores.html';
+}
+
+
+
+startButton.addEventListener('click', function () {
+    var instructions = document.getElementById('instructions');
+    instructions.style.display = 'none';
+    document.getElementById('quiz').style.display = 'inherit';
+    quizTimer();
+    setQuestion();
+})
+
+quizDiv.addEventListener('click', checkAnswer);
+
+
+
+
+
 
 
